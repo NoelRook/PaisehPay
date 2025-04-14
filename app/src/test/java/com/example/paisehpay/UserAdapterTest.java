e2d54647bc47e350f62f11a6588367d0210ec51f
NoelRook <leejingkang6@gmail.com>
2025-04-14 20:00:39 +0800

    refactored database from datatesting branch

---
+++ app/src/test/java/com/example/paisehpay/UserAdapterTest.java
@@ -0,0 +1,4 @@
+package com.example.paisehpay;
+
+public class UserAdapterTest {
+}
---
+++ app/src/test/java/com/example/paisehpay/databaseHandler/ExpenseAdapterTest.java
@@ -0,0 +1,31 @@
+package com.example.paisehpay.databaseHandler;
+
+import junit.framework.TestCase;
+
+public class ExpenseAdapterTest extends TestCase {
+
+    ExpenseAdapter expenseAdapter = new ExpenseAdapter();
+
+    public void testCreate() {
+        // need to run firebase code on local machine to test
+    }
+
+    public void testGet() {
+        //
+    }
+
+    public void testGetByGroupId() {
+    }
+
+    public void testGetByExpenseId() {
+    }
+
+    public void testUpdate() {
+    }
+
+    public void testDelete() {
+    }
+
+    public void testDeleteExpenseByGroupID() {
+    }
+}
